b8ffe827a674387f08bc427afca17cbb60fcbde7
pranavranjan85 <pranavranjankr@gmail.com>
2022-07-25 17:26:52 +0530

    commited by pranav

--- src/test/java/sample_project1/Sample.java
+++ src/test/java/sample_project1/Sample.java
@@ -6,6 +6,7 @@ public class Sample {
 		// TODO Auto-generated method stub
 		System.out.println("hello git");
 		System.out.println("some modification");
+		System.out.println("some modification again");
 
 	}
 
