c3d4e89530e7d8c9d532cf51379eb9452b7583fa
pranavranjan85 <pranavranjankr@gmail.com>
2022-07-25 12:25:25 +0530

    my cimmitment

--- src/test/java/sample_project1/Sample2.java
+++ src/test/java/sample_project1/Sample2.java
@@ -5,6 +5,8 @@ public class Sample2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("for updation");
+		System.out.println("again update");
+		
 
 	}
 
